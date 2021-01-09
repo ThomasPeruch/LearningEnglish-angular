e034c3ce75bbe02f82d8677e2a66a2a0078fec4c
ThomasPeruch <peruchthomas@gmail.com>
2021-01-09 11:53:53 -0300

    phrase model class created and mocked

---
+++ src/app/panel/phrases-mock.ts
@@ -0,0 +1,8 @@
+import { Phrase } from '../shared/phrase.model'
+
+const phrases: Array<Phrase> = [
+    {phraseEng:'I love pizza' , phrasePtBr:'Eu amo pizza'},
+    {phraseEng:'Amazing' , phrasePtBr:'Que demais'},
+    {phraseEng:'I do not like summer' , phrasePtBr:'Eu não gosto do verão'},
+    {phraseEng:'I came to stroll' , phrasePtBr:'Eu vim pra passear'},
+]
