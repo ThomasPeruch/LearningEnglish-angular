743f7ced743d7fd43fc9a7c9798c4b7c28bd93a7
ThomasPeruch <peruchthomas@gmail.com>
2021-01-11 17:36:46 -0300

    Importing phrases-mock in Panel component

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Phrase } from "../shared/phrase.model";
+import { phrases } from "./phrases-mock";
 
 @Component({
   selector: 'app-panel',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelComponent implements OnInit {
 
-  constructor() { }
+  public phrases: Phrase[] = phrases
+
+  constructor() { console.log(this.phrases)}
 
   ngOnInit(): void {
   }
